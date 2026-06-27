3e56c2bba8974469ab0808082d35413a496f801e
Dominic <cristellodominic@gmail.com>
2026-06-27 10:18:04 -0400

    Added Unit tests for all SBOM file types and verified succesfully

--- core/src/test/java/org/eclipse/dash/licenses/tests/SbomFileReaderTests.java
+++ core/src/test/java/org/eclipse/dash/licenses/tests/SbomFileReaderTests.java
@@ -22,18 +22,29 @@ class SbomFileReaderTests {
 
     private static final String AFS_CYCLONEDX_JSON = "/afs-1.0.0-cyclonedx.json";
     private static final String CACHE_PARENT_CYCLONEDX_XML = "/cache-parent-1.1.0-cyclonedx.xml";
+    private static final String SLF4J_SPDX_RDF = "/slf4j-test.spdx.rdf";
+    private static final String SLF4J_SPDX_JSON = "/slf4j-test.spdx.json";
+    private static final String SLF4J_SPDX_TAG_VALUE = "/slf4j-test.spdx";
+    private static final String SLF4J_CYCLONEDX_YAML = "/slf4j-test-cyclonedx.yaml";
+    private static final String SLF4J_SPDX_YAML = "/slf4j-test.spdx.yaml";
 
-    @Test
-    void testV1Format() throws Exception {
+    @Test 
+    void testJsonFormat() throws Exception {
         var input = new File(this.getClass().getResource(AFS_CYCLONEDX_JSON).toURI());
+        //finds the test file on the classpath using the constant path and getResource, then converts it to a URI, then wraps it up as a File 
+        //object to be passed through to SbomFileReader
         SbomFileReader reader = new SbomFileReader(input);
+        //creates a new SbomFileReader pointing to this file
         var expected = Arrays.asList(new String[] {
                 "maven/mavencentral/org.eclipse.serializer/afs/1.0.0",
                 "maven/mavencentral/org.eclipse.serializer/base/1.0.0",
                 "maven/mavencentral/org.slf4j/slf4j-api/1.7.32"
         });
+        //defines the list of expected results that get returned - three purls converted into an IContentId
         var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
+        //calls getContentIds on your reader, convert each IContentId into a string, and then collect them into a list
         assertEquals(expected, found);
+        //compares the expected list to the actual list - if they match it's a success
     }
 
     @Test
@@ -46,4 +57,56 @@ class SbomFileReaderTests {
         var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
         assertEquals(expected, found);
     }
+    
+    @Test
+    void testRdfFormat() throws Exception {
+        var input = new File(this.getClass().getResource(SLF4J_SPDX_RDF).toURI());
+        SbomFileReader reader = new SbomFileReader(input);
+        var expected = Arrays.asList(new String[] {
+                "maven/mavencentral/org.slf4j/slf4j-api/1.7.32"
+        });
+        var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
+        assertEquals(expected, found);
+    }
+    @Test
+    void testSpdxJsonFormat() throws Exception {
+        var input = new File(this.getClass().getResource(SLF4J_SPDX_JSON).toURI());
+        SbomFileReader reader = new SbomFileReader(input);
+        var expected = Arrays.asList(new String[] {
+                "maven/mavencentral/org.slf4j/slf4j-api/1.7.32"
+        });
+        var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
+        assertEquals(expected, found);
+    }
+    @Test
+    void testSpdxTagValueFormat() throws Exception {
+        var input = new File(this.getClass().getResource(SLF4J_SPDX_TAG_VALUE).toURI());
+        SbomFileReader reader = new SbomFileReader(input);
+        var expected = Arrays.asList(new String[] {
+                "maven/mavencentral/org.slf4j/slf4j-api/1.7.32"
+        });
+        var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
+        assertEquals(expected, found);
+    }
+    @Test
+    void testCycloneDxYamlFormat() throws Exception {
+        var input = new File(this.getClass().getResource(SLF4J_CYCLONEDX_YAML).toURI());
+        SbomFileReader reader = new SbomFileReader(input);
+        var expected = Arrays.asList(new String[] {
+                "maven/mavencentral/org.slf4j/slf4j-api/1.7.32"
+        });
+        var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
+        assertEquals(expected, found);
+    }
+    @Test
+    void testSpdxYamlFormat() throws Exception {
+        var input = new File(this.getClass().getResource(SLF4J_SPDX_YAML).toURI());
+        SbomFileReader reader = new SbomFileReader(input);
+        var expected = Arrays.asList(new String[] {
+                "maven/mavencentral/org.slf4j/slf4j-api/1.7.32"
+        });
+        var found = reader.getContentIds().stream().map(each -> each.toString()).collect(Collectors.toList());
+        assertEquals(expected, found);
+    }
+
 }
